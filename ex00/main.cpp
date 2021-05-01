f4bc17709f106945c6b5536b4503aeda2b9f39e2
Your Name <you@example.com>
2021-05-01 12:10:33 +0900

    ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,66 @@
+#include "whatever.hpp"
+#include <iostream>
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	a = 3;
+	b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << " &b = " << &b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << " &::min( a, b ) = " << &::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << " &::max( a, b ) = " << &::max( a, b ) << std::endl;
+	a = 3;
+	b = 2;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::cout << "====================" << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	c = "chaine2";
+	d = "chaine1";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	c = "chaine";
+	d = "chaine";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << " &d = " << &d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << " &::min( c, d ) = " << &::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << " &::max( c, d ) = " << &::max( c, d ) << std::endl;
+
+	std::cout << "====================" << std::endl;
+
+	double e = 42.1;
+	double f = 42.2;
+	::swap(e, f);
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+	e = 42.2;
+	f = 42.2;
+	::swap(e, f);
+	std::cout << "e = " << e << ", f = " << f << " &f = " << &f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << " &::min( e, f ) = " << &::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << " &::max( e, f ) = " << &::max( e, f ) << std::endl;
+	e = 42.2;
+	f = 42.1;
+	::swap(e, f);
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+	return 0;
+}
---
+++ ex00/whatever.hpp
@@ -0,0 +1,21 @@
+template<class T>
+void swap(T& a, T& b)
+{
+	T tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template<class T>
+T &min(T &a, T &b)
+{
+	return (a < b ? a : b);
+}
+
+template<class T>
+T &max(T &a, T &b)
+{
+	return (a > b ? a : b);
+}
